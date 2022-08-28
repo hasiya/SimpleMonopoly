f71cb154ee8ab81f63fd2b01326d5dfb842c0b7d
hasiya <hasith002@yahoo.com>
2022-08-28 17:13:46 +0100

    Just documenting Util.cs and Tile.cs classes.

--- SimpleMonopoly/Tile.cs
+++ SimpleMonopoly/Tile.cs
@@ -1,14 +1,29 @@
 ﻿namespace SimpleMonopoly;
 
+/// <summary>
+/// Tile abstract class.
+/// This class is inherited by all the different Tile classes in the Monopoly Board.  
+/// The class contains a string <c>Name</c> Property 
+/// </summary>
 public abstract class Tile
 {
     public string Name { get; set; }
 
+    /// <summary>
+    /// Constructor of <c>Tile</c> class. 
+    /// </summary>
+    /// <param name="name">A string parameter to initialise the <c>Name</c> Property</param>
     public Tile(string name)
     {
         Name = name;
     }
 
+    /// <summary>
+    /// The <c>TileAction</c> abstract method gets implemented by all the different Tile class that inherits this <c>Tile</c>
+    /// class.
+    /// </summary>
+    /// <param name="player">The <c>Player</c> parameter object to call <c>CurrentPlayer</c> methods.</param>
+    /// <param name="board">The <c>Board</c> parameter object to access the Monopoly board.</param>
     public abstract void TileAction(Player player, Board board);
 
     public override string? ToString()
--- SimpleMonopoly/Util.cs
+++ SimpleMonopoly/Util.cs
@@ -1,7 +1,14 @@
 ﻿namespace SimpleMonopoly;
 
+/// <summary>
+/// The <c>Util</c> static class is just used to print messages to the console. 
+/// </summary>
 public static class Util
 {
+    /// <summary>
+    /// The <c>Print</c> static method prints to console
+    /// </summary>
+    /// <param name="message">A string parameter to get the message to print to the console.</param>
     public static void Print(string message)
     {
         Console.WriteLine(message);
